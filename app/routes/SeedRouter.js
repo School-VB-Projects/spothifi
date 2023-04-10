459dd093dbd6ae1dc5aeda8fda34cb89044dcf63
vbetsch <vic.betsch@gmail.com>
2023-04-11 00:19:06 +0200

    :memo: Add doc to seed route

--- app/routes/SeedRouter.js
+++ app/routes/SeedRouter.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const SeedController = require('../controllers/SeedController');
 const checkJwt = require('../../middlewares/checkJwt');
 
+/**
+ * Response with data generated
+ * @route POST /generate-data
+ * @summary Generating artists, albums, songs and playlists
+ * @group Seed
+ * @return {String} 200 - Generated with success
+ * @returns {String} 500 - An error message
+ */
 router.post('/generate-data', checkJwt, SeedController.generateData);
 
 module.exports = router;
